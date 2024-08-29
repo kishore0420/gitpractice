fb4b6a64cc1a14b8f402ff3aa47e5baf101fba9f
kishore0420 <kishore.king90@gmail.com>
2024-08-29 17:07:21 +0530

    modify1

--- variables.sh
+++ variables.sh
@@ -2,7 +2,7 @@
 person1=ramesh
 person2=suresh
 
-echo "$person1::how are you "$person2 ?"
-echo "$person2:: i am fine $person1"
-echo "$person1:: where are you going $person2 ?"
-echo "$person2":: i am going to hyderabad $person1"
+echo "$person1:: how are you "${person2} ?"
+echo "${person2}:: i am fine $person1"
+echo "$person1:: where are you going ${person2} ?"
+echo "${person2}":: i am going to hyderabad $person1"
