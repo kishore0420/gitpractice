a1826edc3bb0bca1ee9d4ca926f85128ab5f53ce
kishore0420 <kishore.king90@gmail.com>
2024-08-29 16:17:24 +0530

    added variables

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=ramesh
+person2=suresh
+
+echo "$person1: how are you "$person2 ?
+echo "$person2: i am fine $person1"
+echo "$person1: where are you going $person2 ?"
+echo "$person2": i am going to hyderabad $person1 
